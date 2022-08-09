68ae6bc2bdea0a1c2e4b887c4bfd09f73fcffbff
jamestansx <jamestansx@gmail.com>
2022-08-09 18:32:35 +0800

    Fix Trime keep crashing when installing as system app

--- src/customize.sh
+++ src/customize.sh
@@ -3,3 +3,7 @@
 # microG GmsCore needs to be installed as user app for all permissions to be granted
 # see https://github.com/microg/android_packages_apps_GmsCore/issues/1100#issuecomment-711088518
 pm install --dont-kill -g "${MODPATH}/system/priv-app/GmsCore/GmsCore.apk"
+
+# Fix Trime keep crashing as System App
+trime="${MODPATH}/system/app/Trime/Trime.apk"
+[ -f $trime ] && pm install --dont-kill -g $trime
